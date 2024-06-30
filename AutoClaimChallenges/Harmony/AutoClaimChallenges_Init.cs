e063b58b0c79ce47c4e36920bd872f5d04af08fc
unv-annihilator <unv-annihilator@users.noreply.github.com>
2024-06-30 18:57:45 -0500

    AutoClaimChallenges v0.1.0.0

---
+++ AutoClaimChallenges/Harmony/AutoClaimChallenges_Init.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+namespace AutoClaimChallenges;
+
+public class AutoClaimChallenges : IModApi
+{
+    public void InitMod(Mod modInstance)
+    {
+        var harmony = new HarmonyLib.Harmony(GetType().ToString());
+        harmony.PatchAll(Assembly.GetExecutingAssembly());
+    }
+}
---
+++ AutoClaimChallenges/Harmony/Challenges/ChallengeComplete.cs
@@ -0,0 +1,21 @@
+﻿using Challenges;
+using HarmonyLib;
+
+namespace AutoClaimChallenges.Challenges
+{
+    internal static class ChallengeComplete
+    {
+        [HarmonyPatch(typeof(Challenge), nameof(Challenge.HandleComplete))]
+        private static class Challenge_HandleComplete_Patch
+        {
+            private static void Postfix(Challenge __instance)
+            {
+                if (!__instance.ReadyToComplete)
+                    return;
+                __instance.ChallengeState = Challenge.ChallengeStates.Redeemed;
+                __instance.Redeem();
+                QuestEventManager.Current.ChallengeCompleted(__instance.ChallengeClass, true);
+            }
+        }
+    }
+}
