98f5ff00e6c5e5b6902e4c57b6d5f9679a692a56
unv-annihilator <unv-annihilator@users.noreply.github.com>
2024-07-01 00:58:00 -0500

    ToggleSprintFix v0.1.0.0 release

---
+++ SprintToggleFix/Harmony/PlayerMoveControllerPatch/PlayerMoveControllerPatch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+
+namespace SprintToggleFix.PlayerMoveControllerPatch
+{
+    public static class PlayerMoveControllerPatch
+    {
+        [HarmonyPatch(typeof(PlayerMoveController), nameof(PlayerMoveController.Update))]
+        // [HarmonyDebug]
+        private static class PlayerMoveController_Update_Patch
+        {
+            private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var codes = new List<CodeInstruction>(instructions);
+                Log.Out("[ToggleSprintFix] Transpiling PlayerMoveController.Update");
+                for (var i = 0; i < codes.Count; i++)
+                {
+                    if (codes[i].opcode == OpCodes.Ldfld && ((FieldInfo)codes[i].operand).Name == "runInputTime")
+                    {
+                        Log.Out("[ToggleSprintFix] Found runInputTime");
+                        if (codes[i + 1].opcode == OpCodes.Ldc_R4)
+                        {
+                            Log.Out("[ToggleSprintFix] Found 0.2 delay");
+                            var value = (float)codes[i + 1].operand;
+                            if (Math.Abs(value - 0.2f) < 0.000000002f)
+                            {
+                                codes[i + 1] = new CodeInstruction(OpCodes.Ldc_R4, (float)0.0);
+                                Log.Out("[ToggleSprintFix] Updated to 0.0 delay");
+                                break;
+                            }
+                        }
+                    }
+                }
+                return codes.AsEnumerable();
+            }
+        }
+    }
+}
---
+++ SprintToggleFix/Harmony/SprintToggleFix_Init.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using HarmonyLib;
+
+namespace SprintToggleFix
+{
+    public class SprintToggleFix : IModApi
+    {
+        public void InitMod(Mod modInstance)
+        {
+            var harmony = new Harmony(GetType().ToString());
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
