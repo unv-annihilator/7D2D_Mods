c79d56a9b7c814e368a781d13e7b18a9163080fe
unv-annihilator <unv-annihilator@users.noreply.github.com>
2024-07-20 13:20:16 -0500

    Alpha Jump v1.0.1 Grab Behaviour.enabled setter using AccessTools Grab jumpTrigger using AccessTools Additional logging on successful patching Store new code instructions to List to avoid having to reverse inserting Remove unneeded code replacement (replacing code with same code) Utilize temp strings to avoid hardcoded strings Add HarmonyDebug annotation if debug building Ignore TestData/ folder used for validation post update Update solution to build all

--- AlphaJump/Harmony/_EntityPlayerLocal/EntityPlayerLocal_Patches.cs
+++ AlphaJump/Harmony/_EntityPlayerLocal/EntityPlayerLocal_Patches.cs
@@ -11,40 +11,71 @@ namespace AlphaJump._EntityPlayerLocal;
 public class EntityPlayerLocalPatches {
     [HarmonyTranspiler]
     [HarmonyPatch(nameof(EntityPlayerLocal.MoveByInput))]
-    // [HarmonyDebug]
-    private static IEnumerable<CodeInstruction> EntityPlayerLocal_MoveByInput_Transpiler(
-        IEnumerable<CodeInstruction> instructions) {
+#if DEBUG
+    [HarmonyDebug]
+#endif
+    private static IEnumerable<CodeInstruction> EntityPlayerLocal_MoveByInput_Transpiler(IEnumerable<CodeInstruction> instructions) {
+        const string prefix = "[AlphaJump]";
+        var targetStr = $"{typeof(EntityPlayerLocal)}.{nameof(EntityPlayerLocal.MoveByInput)}";
         var codes = new List<CodeInstruction>(instructions);
         var lastBehaviour = -1;
         var endIndex = -1;
-        Log.Out("[AlphaJump]: Transpiling EntityPlayerLocal.MoveByInput");
-        for (var i = 0; i < codes.Count; i++)
-            // IL_02cb: callvirt     instance void [UnityEngine.CoreModule]UnityEngine.Behaviour::set_enabled(bool)
-            if (codes[i].opcode == OpCodes.Callvirt &&
-                (MethodInfo)codes[i].operand == AccessTools.Method(typeof(Behaviour), "set_enabled")) {
+        Log.Out($"{prefix} Transpiling {targetStr}");
+        for (var i = 0; i < codes.Count; i++) {
+            // callvirt     instance void [UnityEngine.CoreModule]UnityEngine.Behaviour::set_enabled(bool)
+            if (codes[i].opcode == OpCodes.Callvirt && (MethodInfo)codes[i].operand == AccessTools.PropertySetter(typeof(Behaviour), nameof(Behaviour.enabled))) {
                 lastBehaviour = i + 2; // Skip current line and next
-            } else if (codes[i].opcode == OpCodes.Stfld && ((FieldInfo)codes[i].operand).Name == "jumpTrigger") {
-                // stfld        bool EntityPlayerLocal::jumpTrigger
-                endIndex = i;
-                Log.Out("[AlphaJump]: Updating EntityPlayerLocal.MoveByInput");
-                // replacing: ldc.i4.1
-                codes[i - 1] = new CodeInstruction(OpCodes.Ldarg_0);
-                codes[i] = new CodeInstruction(OpCodes.Stfld,
-                    AccessTools.Field(typeof(EntityPlayerLocal), nameof(EntityPlayerLocal.jumpTrigger)));
-                codes.Insert(i,
-                    new CodeInstruction(OpCodes.Ldfld,
-                        AccessTools.Field(typeof(MovementInput), nameof(MovementInput.jump))));
-                codes.Insert(i,
-                    new CodeInstruction(OpCodes.Ldfld,
-                        AccessTools.Field(typeof(EntityPlayerLocal), nameof(EntityPlayerLocal.movementInput))));
-                Log.Out("[AlphaJump]: EntityPlayerLocal.MoveByInput - Updated jumpTrigger assignment");
-                break;
             }
 
-        if (endIndex > -1 && lastBehaviour > -1)
+            // stfld        bool EntityPlayerLocal::jumpTrigger
+            if (lastBehaviour == -1 || codes[i].opcode != OpCodes.Stfld || (FieldInfo)codes[i].operand != AccessTools.Field(typeof(EntityPlayerLocal), nameof(EntityPlayerLocal.jumpTrigger))) continue;
+
+            endIndex = i;
+            Log.Out($"{prefix} Patching {targetStr}");
+
+            // replacing:
+            //      ldc.i4.1
+            // with:
+            //      ldarg.0
+            codes[i - 1] = new CodeInstruction(OpCodes.Ldarg_0);
+
+            // New code to insert
+            List<CodeInstruction> newCode = [
+                // ldfld        MovementInput EntityPlayerLocal::movementInput
+                new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(EntityPlayerLocal), nameof(EntityPlayerLocal.movementInput))),
+                // ldfld        bool MovementInput::jump
+                new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(MovementInput), nameof(MovementInput.jump)))
+            ];
+            // Insert code before: stfld        bool EntityPlayerLocal::jumpTrigger
+            codes.InsertRange(i, newCode);
+            Log.Out($"{prefix} {targetStr} - Patched {nameof(EntityPlayerLocal.jumpTrigger)} assignment");
+            break;
+        }
+
+        if (endIndex > -1 && lastBehaviour > -1) {
+            // Remove code starting at `lastBehaviour` for (endIndex - lastBehaviour - 1) count
             codes.RemoveRange(lastBehaviour, endIndex - lastBehaviour - 1);
-        else
-            Log.Error("[AlphaJump]: Failed to patch EntityPlayerLocal.OnUpdateLive");
+            Log.Out($"{prefix} Successfully patched {targetStr}");
+            /*
+            BEFORE:
+                if (this.movementInput.jump && (bool) (UnityEngine.Object) this.vp_FPController && !this.inputWasJump)
+                  this.vp_FPController.enabled = true;
+                if (!this.Jumping && !this.wasJumping && this.movementInput.jump && (this.onGround || this.isLadderAttached) && (UnityEngine.Object) this.AttachedToEntity == (UnityEngine.Object) null)
+                  this.jumpTrigger = true;
+                else if (this.wasLadderAttachedJump && !this.isLadderAttached && this.movementInput.jump && !this.inputWasJump)
+                  this.canLadderAirAttach = true;
+
+            AFTER:
+                if (this.movementInput.jump && (bool) (UnityEngine.Object) this.vp_FPController && !this.inputWasJump)
+                  this.vp_FPController.enabled = true;
+                this.jumpTrigger = this.movementInput.jump;
+                if (this.wasLadderAttachedJump && !this.isLadderAttached && this.movementInput.jump && !this.inputWasJump)
+                  this.canLadderAirAttach = true;
+             */
+        } else {
+            Log.Error($"{prefix} Failed to patch {targetStr}");
+        }
+
         return codes.AsEnumerable();
     }
 }
